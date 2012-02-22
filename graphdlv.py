d8dd97223e49986d8d1c2d6c38a7882375af5f5f
user0 <user0@laptop.(none)>
2012-02-21 22:56:56 -0800

    working subgraph, need implement tuples

--- graphdlv.py
+++ graphdlv.py
@@ -1,6 +1,9 @@
 import re
 import sys
 import pickle
+#debug
+import pprint
+
 
 def main():
 	if(len(sys.argv) < 3):
@@ -14,6 +17,8 @@ def main():
 	s = open(sys.argv[2]).read()
 	f_out.write('digraph{')
 
+	
+	'''
 	styles                =  dict()            #Graphviz styles
 	styles['edge']        = '[color="blue"]'
 	styles['anc']         = '[color="red"]'
@@ -22,12 +27,62 @@ def main():
 	styles['not_lca']     = '[color="purple"]'
 	styles['not not_lca'] = '[color="green"]'
 	styles['lca']         = '[color="orange"]'
+	'''
 	
-	dot(rules, f_out, s, styles)
+	#dot(rules, f_out, s, styles)
+	sub = subgraph(rules, s)
 
 	f_out.write('}')
 	f_out.close()
 
+
+#Creates edge tuples and nodes from aux firings and places into subgraph map
+def subgraph(rules, s):
+	
+	aux_count = 0
+	m = re.findall('(aux[^(]*)\(([^)]*)\)', s)
+	subg = dict()
+	subg['aux'] = set() 
+	for g in m:
+		aux_count = aux_count + 1
+		aux = g[0]
+		aux_terms = g[1].split(',')
+		p_list = rules[aux]
+		aux_atom = '%s(%s)' % (aux, g[1])
+		subg['aux'].add(aux_atom)
+
+		# Aux -> Head
+		head = p_list[0][0] 
+		atom = head + '('
+		term_i = p_list[0][1]
+		for i in term_i:
+			atom = atom + aux_terms[i] + ','
+		atom = atom.rstrip(',') + ')'
+		if head not in subg:
+			subg[head] = {'nodes': set(), 'edges':[]}
+		subg[head]['nodes'].add(atom)
+		subg[head]['edges'].append('"%s" -> "%s"' % (aux_atom, atom))
+		
+		# Body -> Aux
+		for t in p_list[1:]:
+			pred = t[0]
+			term_i = t[1]
+			atom = pred + '('
+			for i in term_i:
+				atom = atom + aux_terms[i] + ','
+			atom = atom.rstrip(',') + ')'
+
+			if pred not in subg:
+				subg[pred] = {'nodes':set(), 'edges':[]}
+			subg[pred]['nodes'].add(atom)
+			subg[pred]['edges'].append('"%s" -> "%s"' % (atom, aux_atom))
+
+	pp = pprint.PrettyPrinter(indent=4)
+	pp.pprint(subg)	
+
+	print('There were %s firings.' % aux_count)
+	return subg;
+	
 
 #Outputs .dot file from aux firings data
 def dot(rules, f_out, s, styles):
