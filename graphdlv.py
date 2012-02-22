377944e191301486f66811177f42e36506b25538
user0 <user0@laptop.(none)>
2012-02-22 01:02:01 -0800

    commit

--- graphdlv.py
+++ graphdlv.py
@@ -1,55 +1,38 @@
 import re
 import sys
 import pickle
+import pygraphviz as pgv
 #debug
 import pprint
 
-
 def main():
 	if(len(sys.argv) < 3):
-		print('usage: python parse.py [parse map] [dlv output] \n')
+		print('usage: python parse.py [parse map] [dlv output] [styles] \n')
 		sys.exit(1)
-	f_out = open('graph_dlv.dot', 'w')
-	#f_in = open(sys.argv[1]).readlines()
-	#rules = pickle.load(open('parse_map.p', 'rb'))
+	f_out = 'graph_dlv.pdf' 
 	rules = pickle.load(open(sys.argv[1], 'rb'))
 	
 	s = open(sys.argv[2]).read()
-	f_out.write('digraph{')
 
-	
-	'''
-	styles                =  dict()            #Graphviz styles
-	styles['edge']        = '[color="blue"]'
-	styles['anc']         = '[color="red"]'
-	styles['com_anc']     = '[color="yellow"]'
-	styles['aux']         = '[shape=point]'
-	styles['not_lca']     = '[color="purple"]'
-	styles['not not_lca'] = '[color="green"]'
-	styles['lca']         = '[color="orange"]'
-	'''
-	
-	#dot(rules, f_out, s, styles)
-	sub = subgraph(rules, s)
-
-	f_out.write('}')
-	f_out.close()
+	g = graph_map(rules, s)
+	styles = read_styles(sys.argv[3])
+	draw(g, styles, f_out)
 
 
-#Creates edge tuples and nodes from aux firings and places into subgraph map
-def subgraph(rules, s):
+#Creates edge tuples and nodes from aux firings and places into graph map
+def graph_map(rules, s):
 	
 	aux_count = 0
 	m = re.findall('(aux[^(]*)\(([^)]*)\)', s)
 	subg = dict()
-	subg['aux'] = set() 
+	subg['aux'] = {'nodes':set(), 'edges':[]} 
 	for g in m:
 		aux_count = aux_count + 1
 		aux = g[0]
 		aux_terms = g[1].split(',')
 		p_list = rules[aux]
 		aux_atom = '%s(%s)' % (aux, g[1])
-		subg['aux'].add(aux_atom)
+		subg['aux']['nodes'].add(aux_atom)
 
 		# Aux -> Head
 		head = p_list[0][0] 
@@ -61,7 +44,7 @@ def subgraph(rules, s):
 		if head not in subg:
 			subg[head] = {'nodes': set(), 'edges':[]}
 		subg[head]['nodes'].add(atom)
-		subg[head]['edges'].append('"%s" -> "%s"' % (aux_atom, atom))
+		subg[head]['edges'].append((aux_atom, atom))
 		
 		# Body -> Aux
 		for t in p_list[1:]:
@@ -75,51 +58,34 @@ def subgraph(rules, s):
 			if pred not in subg:
 				subg[pred] = {'nodes':set(), 'edges':[]}
 			subg[pred]['nodes'].add(atom)
-			subg[pred]['edges'].append('"%s" -> "%s"' % (atom, aux_atom))
+			subg[pred]['edges'].append((atom, aux_atom))
 
 	pp = pprint.PrettyPrinter(indent=4)
 	pp.pprint(subg)	
 
 	print('There were %s firings.' % aux_count)
 	return subg;
+
+def read_styles(f_in):
+	styles = dict()
+	s = open(f_in,'rb').readlines() 
+	for line in s:
+		line = line.split(':')
+		head = line[0]
+		attr = dict()
+		m = re.findall('([^=]*)=(\w*)',line[1])
+		for g in m:
+			attr[g[0]] = g[1]
 	
 
-#Outputs .dot file from aux firings data
-def dot(rules, f_out, s, styles):
+def draw(graph, styles, f_out):
+	G = pgv.AGraph()
+	for key,subg in graph.iteritems():
+		attr = styles[key]
+		G.add_nodes_from(subg['nodes'], **attr)
+		G.add_edges_from(subg['edges'], **attr)
+	G.draw(f_out,prog='dot')
+
 	
-	#aux_count = dict()
-	aux_count = 0
-
-	m = re.findall('(aux[^(]*)\(([^)]*)\)', s)
-	for g in m:
-		aux_count = aux_count + 1
-		aux = g[0]
-		aux_terms = g[1].split(',')
-		p_list = rules[aux]
-		f_out.write('"%s(%s)" %s\n' % (aux, g[1], styles['aux']))
-
-		# Aux -> Head
-		p = p_list[0][0]
-		term_i = p_list[0][1]
-		f_out.write('"%s(%s)" -> "%s(' % (aux, g[1], p))
-		for i in term_i:
-			f_out.write(aux_terms[i] + ',')
-		f_out.write(')" %s\n' % styles[p])
-			
-		# Body -> Aux
-		for t in p_list[1:]:
-			p = t[0]
-			term_i = t[1]
-			f_out.write('"%s(' % p)
-			for i in term_i:
-				f_out.write(aux_terms[i] + ',')
-			if styles[p]:
-				f_out.write(')" -> "%s(%s)" %s\n' % (aux, g[1], styles[p]))
-			else:
-				f_out.write(')" -> "%s(%s)" %s\n' % (aux, g[1]))
-				
-	print('There were %s firings.' % aux_count)
-
-
 if __name__=="__main__":
 	main()
